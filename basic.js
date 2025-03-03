8dc900930dd61197e10e0a88f496d500ae73c102
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-03 10:33:08 +0530

    created a repo

---
+++ basic.js
@@ -0,0 +1,42 @@
+// declaration of strings
+
+// let str1=new String("The Software Engineering");
+
+// let str2="The Web development";  //Double quotes
+// let str3='The Web development';  //Single quotes
+// let str4=`The Web development`;  //backtick
+// console.log(str1,str2,str3,str4)
+
+// Use of backticks
+let strr="world";
+console.log`This is a beautiful ${strr} yeah`;
+//output 🙄- [ 'This is a beautiful ', ' yeah' ] world 
+
+//String Length
+let str="This is a string";
+console.log(str.length);
+
+//Accessing specific character
+console.log(str[2]);
+console.log(str.charAt(2));
+
+
+// Looping through String
+for(let i=0;i<str.length;i++){
+    console.log(str[i])
+}
+
+
+
+// Modifying String
+str[3]="t";
+console.log(str)  // Does not work because string in js is immutable
+// instead we use
+console.log(str.replace("s","t"))
+console.log(str.replaceAll("i","z"));
+
+
+//Convert object into string
+let obj={name:"vishal"};
+console.log(typeof JSON.stringify(obj))
+
---
+++ reverse-string.js
@@ -0,0 +1,15 @@
+function reverseString(s) {
+    let i = 0;
+    let j = s.length-1;
+    while (i <= j) {
+        let temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+    return s
+}
+
+let s="ask";
+console.log(reverseString(s));
