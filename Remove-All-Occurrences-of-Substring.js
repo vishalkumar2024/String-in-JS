a25fffb5bdbb9e4a6efd80619ce914b1e1957643
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-06 14:18:37 +0530

    added one file

---
+++ Remove-All-Occurrences-of-Substring.js
@@ -0,0 +1,20 @@
+function removeOccurrence(str, part) {
+    let n = str.length;
+    function remove(str, part) {
+        return str.replace(part, "");
+    }
+    while (n > 0 && str.includes(part)) {
+       str=  remove(str, part);
+    }
+    return str;
+}
+
+let str = "daabcbaabcbc";
+let part = "abc";
+// // Output: "dab";
+console.log(removeOccurrence(str, part));
+
+// TC=O(m*n) //  if str has M occurrences of part, the
+//  loop will run M times.
+
+//SC=O(1)
