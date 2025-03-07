ad98970aa030bbee76ff1185235c1ef9e5412f9c
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-07 13:07:42 +0530

    added one file

---
+++ Reverse-word.js/better.js
@@ -0,0 +1,20 @@
+function reverse(s) {
+    let str = s.trim().split(/\s+/); //Tc=O(n)
+    console.log(str)
+    let i = 0;
+    let j = str.length - 1;
+    while (i < j) {   //TC=O(n/2)
+        let temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+    return str.join(" ");
+}
+s = "a good   example"
+// Output: "example good a"
+console.log(reverse(s));
+
+// TC=O(n)
+// SC=O(n) - because of str array
