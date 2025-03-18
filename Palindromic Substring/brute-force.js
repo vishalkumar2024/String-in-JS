f52bb4bfa21e793b82ddde82f47ce5900c0a9b62
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-18 11:12:24 +0530

    modify

--- Palindromic Substring/brute-force.js
+++ Palindromic Substring/brute-force.js
@@ -1,21 +1,28 @@
+// This fucntion returns count all palindromic sub-strings present in the string.
+//  The length of the palindromic sub-string must be greater than or equal to 2.
+
 function palindromicSubstring(s) {
     let n = s.length;
     function isPalindrome(str) {
-        let rev = "";
-        for (let i = str.length - 1; i >= 0; i--) {
-            rev += str[i];
+        let i=0;
+        let j=str.length-1;
+        while(i<=j){
+            if(str[i]!=str[j]){
+                return false
+            }
+            i++;
+            j--;
         }
-        return true ? rev === str : false;
+        return true;
     }
     let count = 0;
-    for (let i = 0; i < n; i++) {
-        let res = "";
-        for (let j = i; j < n; j++) {
+    for (let i = 0; i < n; i++) {   //
+        let res = "";   //SC=O(n)
+        for (let j = i; j < n; j++) {  //TC=O(n(n+1)/2) ~ O(n*n)
             res += s[j];
             if (isPalindrome(res)) {
-                if(res.length>1){
-                count++;
-                console.log(res)
+                if (res.length > 1) {
+                    count++;
                 }
             }
         }
@@ -24,3 +31,6 @@ function palindromicSubstring(s) {
 }
 let s = "abaab";
 console.log(palindromicSubstring(s));
+
+// TC=O(n*n*n)
+// SC=O(n)
