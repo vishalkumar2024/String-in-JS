f3a1a97825bc3a7202b6f362044c512fe7a5700e
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-21 12:28:22 +0530

    added one file

---
+++ Roman-to-Integer.js
@@ -0,0 +1,38 @@
+function Roman(s) {
+    function number(n) {
+        if (n == "I") {
+            return 1
+        }
+        else if (n == "V") {
+            return 5
+        }
+        else if (n == "X") {
+            return 10
+        }
+        else if (n == "L") {
+            return 50
+        }
+        else if (n == "C") {
+            return 100
+        }
+        else if (n == "D") {
+            return 500
+        }
+        else if (n == "M") {
+            return 1000
+        }
+    }
+    let res = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        if (number(s[i]) < number(s[i + 1])) {
+            res -= number(s[i]);
+        }
+        else {
+            res += number(s[i]);
+        }
+    }
+    res += number(s[s.length - 1])
+    return res;
+}
+let s = "XLIII"
+console.log(Roman(s));
