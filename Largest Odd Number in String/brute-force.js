e869e327d6fcc002206ef2a7acbc72d27879c7d6
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-06-24 20:25:25 +0530

    did some modifications

--- Largest Odd Number in String/brute-force.js
+++ Largest Odd Number in String/brute-force.js
@@ -1,17 +1,18 @@
 // leetcode 1903
 function largestOddNumber(num) {
+    if (num[num.length - 1] % 2 !== 0) {
+        return num
+    }
     let res = ""
-    let longestRes="";
     for (let i = num.length - 1; i >= 0; i--) {
         if (num[i] % 2 !== 0) {
-            for(let k=0;k<=i;k++){
-               res+=num[k]
+            for (let k = 0; k <= i; k++) {
+                res += num[k]
             }
-            longestRes=res.length>longestRes.length?res:longestRes
-            res=""
+            break;
         }
     }
-    return longestRes;
+    return res;
 };
 
 let num = "925346"
